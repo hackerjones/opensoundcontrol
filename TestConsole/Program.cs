6d9e6bd070f0d410736471b19b48323987c6efb7
Mark Jones <markalanj@gmail.com>
2010-07-09 15:43:43 -0500

    Successful test
    Make Controller with two servos in pan and tilt configuration

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -11,14 +11,23 @@ namespace TestConsole
         {
             var udp = new OscUdpIoDevice(10000);
             var addr = new OscIoDeviceAddress(OscIoDeviceAddressType.Udp,
-                                              new IPEndPoint(IPAddress.Parse("192.168.0.103"), 10000));
-            var p1 = new OscMessage("/servo/0");
+                                              new IPEndPoint(IPAddress.Parse("192.168.0.106"), 10000));
+            
             udp.SendCompleted += SendCompleted;
             udp.ReceiveCompleted += ReceiveCompleted;
-            udp.Send(p1, addr);
+
+            var p1 = new OscMessage("/servo/0/position 512");            
+            var p2 = new OscMessage("/servo/1/position 512");
+            var p3 = new OscMessage("/servo/0/position 0");
+            var p4 = new OscMessage("/servo/1/position 0");
 
             while (true)
             {
+                udp.Send(p1, addr);
+                udp.Send(p2, addr);
+                Thread.Sleep(1000);
+                udp.Send(p3, addr);
+                udp.Send(p4, addr);
                 Thread.Sleep(1000);
             }
         }
@@ -33,12 +42,12 @@ namespace TestConsole
                                              OscIoDeviceEventArgs args)
         {
             Console.WriteLine("Receive from {0}", args.DeviceAddress.IPEndPoint);
-            if (args.HasBundle)
+            if (args.ContainsBundle)
             {
                 Console.WriteLine(args.Bundle.ToString());                
             }
             
-            if (args.HasMessage)
+            if (args.ContainsMessage)
             {
                 
             }
